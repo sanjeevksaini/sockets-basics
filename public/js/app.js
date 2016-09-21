1c6334e2f75d66f9481bb2cc57106a39b6b34258
Sanjeev Kumar <sanjeevksaini@yahoo.co.in>
2016-09-21 19:14:00 +0800

    add broadcast

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,10 @@ var socket = io();
 
 socket.on('connect', function(){
 	console.log("Connected to socket.io server");
+
+});
+
+socket.on('message',function(message){
+	console.log('New message');
+	console.log(message.text);
 });
